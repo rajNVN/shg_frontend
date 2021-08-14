16774a5e957330bbbd8dfc29c2b5c9c3ec23f2cd
raj_NVN <rajNVN@users.noreply.github.com>
2021-08-15 00:50:16 +0530

    adding init react project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'antd/dist/antd.css';
+import './App.css';
+import {Button} from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
